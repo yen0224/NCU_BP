faf3ea43534f2c46c72d1a69d0d8f34ef51cfd36
yen <yen0224@pm.me>
2022-01-04 22:12:54 +0800

    update Hw15.c

--- 110403041_HW15.c
+++ 110403041_HW15.c
@@ -1,26 +1,21 @@
 #include <stdio.h>
 #include <string.h>
+struct info
+    {
+        char name[10];
+        float score;
+        int ifpass;
+    };
 int main(int argc, char const *argv[])
 {
     int maxscore = 0, index = 0;
     float sum = 0.0;
     int allpass = 1;
-    int manymax=0;
-    struct info
-    {
-        char name[10];
-        float score;
-        int ifpass;
-    } student[5];
-    // input, calculate the averge score, and find who get the max score
+    
+    struct info student[5]={{"Ricky",95,0},{"Amy",70,0},{"Eason",20,0},{"Allen",80,0},{"Sean",99,0}};
+    // calculate the averge score, and find who get the max score
     for (int i = 0; i < 5; i++)
     {
-        printf("student's name[%d]:", i + 1);
-        fgets(student[i].name, 10, stdin);
-        student[i].name[strlen(student[i].name)-1]='\0';
-        printf("%s's score is:",student[i].name);
-        scanf("%f", &student[i].score);
-        
         sum += student[i].score;
         if (student[i].score > maxscore)
             index = i;
@@ -34,7 +29,6 @@ int main(int argc, char const *argv[])
             student[i].ifpass = 0;
             allpass=0;
         }
-        getchar();
     }
     printf("\n%s got the highest score %.2f amount all the students in this exam.\n", student[index].name, student[index].score);
     printf("\nThe following part lists the students who didn't pass the exam:\n");
